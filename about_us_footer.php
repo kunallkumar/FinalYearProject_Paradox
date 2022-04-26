b4e5ae3650c9c9a6097980638cbbdf677c82695d
kunallkumar <kk74987@gmail.com>
2022-04-26 23:28:12 +0530

    first commit

---
+++ about_us_footer.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		About Us
+	</title>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/about_style.css">
+	<link rel="stylesheet" href="bootstrap\bootstrap.min.css">
+	    <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<!--About Us-->
+<div class="team-section">
+	<h1>Our Team</h1>
+	<span class=border></span>
+	<div class="ps">
+		<a href="#p1"><img src="image/merc.jpg" alt="" style="margin-top:-5px"></a>
+		<a href="#p2"><img src="Image/kashish.jpeg" alt="" style="margin-top:-65px"></a>
+		<a href="#p3"><img src="image/nikhil.jpeg" alt="" style="margin-top:"></a>
+		
+	</div>
+
+<div class="section " id="p1">
+	<span class="name">Kunal</span>
+	<span class="border"></span>
+	<p>
+	Hi ! Myself Kunal Kumar and I played role as a web developer and database designer. I do have knowledge in web designing. I have code the front-end and back-end to make the website workable. I used HTML, CSS, Html5, Bootstrap for layout designing. For back-end MySQL is used. I used PHP, javascript, ajax to make the website responsible. As it is an ecommerce website, so its economical feasibility is properly studied and then get structured this project. 
+	</p>
+
+</div>
+<div class="section " id="p2">
+	<span class="name">Kashish</span>
+	<span class="border"></span>
+	<p>
+	 Hello viewers! Myself Kashish Chauhan and I worked as a front-end designer and content collector. I can do website design, logo design, banner design and much more.
+	 Also helped in planning and directing the project work. Also I have prepared documentation for this project. The content selection is a very tedious and creative task to make a website impressive. I have supervised the image collection for the data keeping and desined the database by using DFD and physical structure. I can do web site development, ecommerce website, and much more.
+	</p>
+
+</div>
+<div class="section " id="p3">
+	<span class="name">Nikhil</span>
+	<span class="border"></span>
+	<p>
+	Hey Its Nikhil Kohli there! I contribute as a team lead and database designer. I do have a good knowledge in web design and backend development. I have code the front-end and back-end to make the website workable. I used HTML, CSS, Html5, Bootstrap for layout designing.For back-end MySQL is used. I used PHP, javascript, ajax to make the website responsible. Also I perform a managerial role during project development. I tried to attached few of my knowledge in this project.	</p>
+
+</div>
+
+
+
+
+
+
+</div>
+</body>
+</html>
